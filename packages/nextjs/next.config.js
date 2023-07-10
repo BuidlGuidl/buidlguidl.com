bb7eac15613c8f886c3768a020c5a947921997ca
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-10 12:01:01 +0200

    Redirections

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,6 +9,25 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
+  async redirects() {
+    return [
+      {
+        source: "/builders/:address",
+        destination: "https://app.buidlguidl.com/builders/:address",
+        permanent: true,
+      },
+      {
+        source: "/build/:buildId",
+        destination: "https://app.buidlguidl.com/build/:buildId",
+        permanent: true,
+      },
+      {
+        source: "/activity",
+        destination: "https://app.buidlguidl.com/activity",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
