db120486ea86850b8488d706c57703949a0bedac
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-10 19:00:41 +0200

    UI: hero + first section (#1)
    Co-authored-by: Shiv Bhonde <shivbhonde04@gmail.com>

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,13 +1,44 @@
 import Image from "next/image";
 import type { NextPage } from "next";
+import { Header } from "~~/components/Header";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <div className="">
-        <Image src="/assets/hero.png" alt="BuidlGuidl hero image" className="absolute z-0 object-cover" fill />
+      {/* Hero section with header */}
+      <div className="bg-[url(/assets/hero.png)] min-h-screen bg-cover bg-center">
+        <Header />
+        <div className="flex justify-center">
+          <h1 className="text-center text-4xl md:text-6xl mt-8 max-w-md md:max-w-lg font-bold">
+            Welcome to the BuidlGuidl
+          </h1>
+        </div>
+      </div>
+      {/* Star Building on Ethereum */}
+      <div className="bg-white flex justify-center items-center bg-[url(/assets/sre-path.png)] bg-bottom bg-repeat-x px-7">
+        <div className="flex flex-col-reverse md:flex-row items-center justify-between w-full md:w-3/4 2xl:max-w-7xl py-16 md:py-28">
+          <div className="flex items-center md:items-start flex-col space-y-5">
+            <h1 className="text-3xl md:text-left md:text-6xl font-semibold w-3/4">Start Building on Ethereum</h1>
+            <ul className="list-disc list-inside flex flex-col space-y-3">
+              <li>7 start to finish tutorials </li>
+              <li>Learn from the best Ethereum developers (+ guidance)</li>
+              <li>Level Up your skill and get paid</li>
+            </ul>
+            <a
+              href="https://speedrunethereum.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-accent btn-md md:self-start px-8"
+            >
+              Get Started Now!
+            </a>
+          </div>
+          <div>
+            <Image src="/assets/sre-forest.png" alt="hero" width={600} height={600} />
+          </div>
+        </div>
       </div>
     </>
   );
--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -8,13 +8,13 @@ module.exports = {
     themes: [
       {
         scaffoldEth: {
-          primary: "#93BBFB",
-          "primary-content": "#212638",
-          secondary: "#DAE8FF",
-          "secondary-content": "#212638",
-          accent: "#93BBFB",
-          "accent-content": "#212638",
-          neutral: "#212638",
+          primary: "#808CFF",
+          "primary-content": "#182232",
+          secondary: "#B5DC3A",
+          "secondary-content": "#182232",
+          accent: "#FF6E1D",
+          "accent-content": "#182232",
+          neutral: "#FFFAFA",
           "neutral-content": "#ffffff",
           "base-100": "#ffffff",
           "base-200": "#f4f8ff",
@@ -59,58 +59,13 @@ module.exports = {
           },
         },
       },
-      {
-        exampleUi: {
-          primary: "#000000",
-          "primary-content": "#ffffff",
-          secondary: "#FF6644",
-          "secondary-content": "#212638",
-          accent: "#93BBFB",
-          "accent-content": "#212638",
-          neutral: "#f3f3f3",
-          "neutral-content": "#212638",
-          "base-100": "#ffffff",
-          "base-200": "#f1f1f1",
-          "base-300": "#d0d0d0",
-          "base-content": "#212638",
-          info: "#93BBFB",
-          success: "#34EEB6",
-          warning: "#FFCF72",
-          error: "#FF8863",
-
-          "--rounded-btn": "1rem",
-
-          ".tooltip": {
-            "--tooltip-tail": "6px",
-          },
-        },
-      },
     ],
   },
   theme: {
     // Extend Tailwind classes (e.g. font-bai-jamjuree, animate-grow)
     extend: {
       fontFamily: {
-        "bai-jamjuree": ["Bai Jamjuree", "sans-serif"],
-      },
-      keyframes: {
-        grow: {
-          "0%": {
-            width: "0%",
-          },
-          "100%": {
-            width: "100%",
-          },
-        },
-        zoom: {
-          "0%, 100%": { transform: "scale(1, 1)" },
-          "50%": { transform: "scale(1.1, 1.1)" },
-        },
-      },
-      animation: {
-        grow: "grow 5s linear infinite",
-        "pulse-fast": "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
-        zoom: "zoom 1s ease infinite",
+        "space-grotesk": ["Space Grotesk", "sans-serif"],
       },
     },
   },
