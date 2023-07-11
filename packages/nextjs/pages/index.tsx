2c8aebb02631797ab926166c19ec67a8e1185a42
Carlos Sánchez <oceanrdn@gmail.com>
2023-07-11 10:37:03 +0200

    Cleanup classes on first section

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -12,83 +12,82 @@ const Home: NextPage = () => {
       <div className="bg-[url(/assets/hero.png)] min-h-screen bg-cover bg-center">
         <Header />
         <div className="flex justify-center">
-          <h1 className="text-center text-4xl md:text-6xl mt-8 max-w-md md:max-w-lg font-bold">
+          <h1 className="text-center text-4xl lg:text-6xl mt-8 max-w-md lg:max-w-lg font-bold">
             Welcome to the BuidlGuidl
           </h1>
         </div>
       </div>
       {/* Star Building on Ethereum */}
       <div className="bg-white">
-        <div className="flex justify-center items-center px-4">
-          <div className="flex flex-col-reverse md:flex-row items-center justify-between w-full md:w-11/12 2xl:max-w-7xl py-16 md:py-28 md:px-12">
-            <div className="flex items-center md:items-start flex-col space-y-5">
-              <h1 className="text-3xl md:text-left md:text-6xl font-semibold w-3/4">Start Building on Ethereum</h1>
-              <ul className="list-disc list-inside flex flex-col space-y-3">
-                <li>7 start to finish tutorials </li>
-                <li>Learn from the best Ethereum developers (+ guidance)</li>
-                <li>Level Up your skill and get paid</li>
-              </ul>
+        <div className="container max-w-[90%] lg:max-w-5xl m-auto py-16 lg:py-28 lg:px-12 flex flex-col-reverse lg:flex-row items-center gap-5 lg:gap-0">
+          <div className="space-y-6">
+            <h1 className="text-3xl lg:text-6xl font-semibold lg:w-3/4 text-center lg:text-left">
+              Start Building on Ethereum
+            </h1>
+            <ul className="list-disc list-inside flex flex-col space-y-3 m-auto lg:mx-0 max-w-[300px] lg:max-w-none">
+              <li>7 start to finish tutorials </li>
+              <li>Learn from the best Ethereum developers (+ guidance)</li>
+              <li>Level Up your skill and get paid</li>
+            </ul>
+            <div className="text-center lg:text-left">
               <a
                 href="https://speedrunethereum.com/"
                 target="_blank"
                 rel="noreferrer"
-                className="btn btn-accent btn-md md:self-start px-8"
+                className="btn btn-accent btn-md lg:self-start px-8"
               >
                 Get Started Now!
               </a>
             </div>
-            <div>
-              <Image src="/assets/sre-forest.png" alt="hero" width={600} height={600} />
-            </div>
+          </div>
+          <div className="max-w-[300px] lg:max-w-none">
+            <Image src="/assets/sre-forest.png" alt="hero" width={600} height={600} />
           </div>
         </div>
-        <div className="bg-[url(/assets/sre-path.png)] bg-repeat-x h-32 relative"></div>
+        <div className="bg-[url(/assets/sre-path.png)] bg-repeat-x h-32 relative bg-[35%_top]"></div>
       </div>
       {/* Test Your knowledge */}
-      <div className="flex bg-base-100 -mt-16">
-        <div className="flex justify-center items-center px-4 w-full">
-          <div className="flex flex-col items-center justify-between w-full md:w-11/12 2xl:max-w-7xl py-16 md:py-28 md:px-12">
-            <div className="flex flex-col-reverse md:flex-row md:self-start w-4/5 items-center">
-              <div className="flex flex-col items-center md:items-start mt-6 md:mt-0">
-                <h1 className="text-3xl md:text-5xl font-semibold text-center md:text-left">Test Your Knowledge</h1>
-                <p className="text-center md:text-left w-3/4">
-                  Copy about: Learn that you don’t know what you don’t know. you need to go through tour of duty + SRE
-                  gets you into the BG ?
-                </p>
-              </div>
-              <div>
-                <Image src="/assets/dice.png" alt="hero" width={225} height={225} />
-              </div>
+      <div className="flex justify-center px-4 w-full bg-base-100 -mt-16">
+        <div className="flex flex-col items-center justify-between w-full lg:w-11/12 2xl:max-w-7xl py-16 lg:py-28 lg:px-12">
+          <div className="flex flex-col-reverse lg:flex-row lg:self-start w-4/5 items-center">
+            <div className="flex flex-col items-center lg:items-start mt-6 lg:mt-0">
+              <h1 className="text-3xl lg:text-5xl font-semibold text-center lg:text-left">Test Your Knowledge</h1>
+              <p className="text-center lg:text-left w-3/4">
+                Copy about: Learn that you don’t know what you don’t know. you need to go through tour of duty + SRE
+                gets you into the BG ?
+              </p>
             </div>
-            {/*  */}
-            {/* Cards container */}
-            <div className="flex flex-wrap md:space-x-5 items-start justify-center">
-              {/* Card */}
-              <Card
-                num={4}
-                name={"Build a DEX"}
-                src={"/assets/chall-dex.png"}
-                description={
-                  "💵 Build an exchange that swaps ETH to tokens and tokens to ETH. 💰 This is possible because"
-                }
-              />
-              <Card
-                num={6}
-                name={"Multi-Sig Wallet"}
-                src={"/assets/chall-multisig.png"}
-                description={
-                  "👩‍👩‍👧‍👧 Using a smart contract as a wallet we can secure assets by requiring multiple accounts to 'vote' on transactions."
-                }
-              />
-              <Card
-                num={5}
-                name={"State Channels"}
-                src={"/assets/chall-state.png"}
-                description={
-                  "🐌 The Ethereum blockchain has great decentralization & security properties. These properties come at a price"
-                }
-              />
+            <div>
+              <Image src="/assets/dice.png" alt="hero" width={225} height={225} />
             </div>
+          </div>
+          {/* Cards container */}
+          <div className="flex flex-wrap lg:space-x-5 items-start justify-center">
+            {/* Card */}
+            <Card
+              num={4}
+              name={"Build a DEX"}
+              src={"/assets/chall-dex.png"}
+              description={
+                "💵 Build an exchange that swaps ETH to tokens and tokens to ETH. 💰 This is possible because"
+              }
+            />
+            <Card
+              num={6}
+              name={"Multi-Sig Wallet"}
+              src={"/assets/chall-multisig.png"}
+              description={
+                "👩‍👩‍👧‍👧 Using a smart contract as a wallet we can secure assets by requiring multiple accounts to 'vote' on transactions."
+              }
+            />
+            <Card
+              num={5}
+              name={"State Channels"}
+              src={"/assets/chall-state.png"}
+              description={
+                "🐌 The Ethereum blockchain has great decentralization & security properties. These properties come at a price"
+              }
+            />
           </div>
         </div>
       </div>
