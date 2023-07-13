055641da0ff2169e072a81718c7d87eb034f18d7
Shiv Bhonde <shivbhonde04@gmail.com>
2023-07-13 17:41:22 +0530

    complete learn-more and stats section

---
+++ packages/nextjs/components/LearnMoreCard.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export const LearnMoreCard = ({
+  name,
+  description,
+  src,
+  link,
+}: {
+  name: string;
+  description: string;
+  src: string;
+  link: string;
+}) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noreferrer"
+      className="flex flex-col lg:w-[30%] max-w-xs shadow-lg bg-white rounded-[42px] p-5 mt-4"
+    >
+      <div className="flex w-full h-[200px] relative">
+        <Image src={src} fill alt={name} className="w-full object-contain" />
+      </div>
+      <div className="mb-4">
+        <h3 className="text-lg m-0 mb-2">{name}</h3>
+        <p className="text-gray-700 m-0 text-sm">{description}</p>
+      </div>
+    </a>
+  );
+};
