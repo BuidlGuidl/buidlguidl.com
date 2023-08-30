dc25e94971694cee68c388d3e455ec7b20b5aa88
Shiv Bhonde <shivbhonde04@gmail.com>
2023-08-30 10:57:28 +0530

    add padding-bottom to Header on mobile screens

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -8,7 +8,7 @@ import TrackedLink from "~~/components/TrackedLink";
  */
 export const Header = () => {
   return (
-    <div className="navbar min-h-0 flex-shrink-0 justify-between px-0 py-4 sm:px-2 bg-[#EFFBCA]">
+    <div className="navbar min-h-0 flex-shrink-0 justify-between px-0 py-4 pb-8 md:pb-4 sm:px-2 bg-[#EFFBCA]">
       <div className="navbar-start w-auto lg:w-1/2">
         <Link href="/" passHref className="flex items-center gap-2 ml-4 mr-6">
           <div className="flex relative w-[130px] md:w-[150px] h-[36px]">
