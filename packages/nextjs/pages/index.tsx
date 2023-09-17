69453a640f282ff364121b4fcfc0a77bcdf61a6c
tokodev <pablo@asdloop.com>
2023-09-18 01:27:15 +0200

    Fix: Align cohorts table titles to content on small resolutions

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -288,9 +288,9 @@ const Home: NextPage<{ stats: Stats; cohortsData?: Cohort[] }> = ({ stats, cohor
               <table className="min-w-full">
                 <thead>
                   <tr className="text-base">
-                    <th className="bg-base-100 text-left py-3 px-8">Name</th>
-                    <th className="bg-base-100 text-left py-3 px-8">Hackers</th>
-                    <th className="bg-base-100 text-left py-3 px-8">ETH</th>
+                    <th className="bg-base-100 text-left py-3 px-4 xl:px-8">Name</th>
+                    <th className="bg-base-100 text-left py-3 px-4 xl:px-8">Hackers</th>
+                    <th className="bg-base-100 text-left py-3 px-4 xl:px-8">ETH</th>
                   </tr>
                 </thead>
                 <tbody className="shadow-custom rounded-3xl text-sm ">
