1b1204eebc6b3ca918cfda14802541f66ba69309
Shiv Bhonde <shivbhonde04@gmail.com>
2023-09-20 20:48:00 +0530

    update padding of supporting dev to match other sections

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -238,7 +238,7 @@ const Home: NextPage<{
 
       {/* Supporting Devs*/}
       <div className="bg-base-300">
-        <div className="container max-w-[90%] lg:max-w-7xl m-auto py-16 lg:py-20 lg:pl-24 flex flex-col-reverse lg:flex-row items-center gap-5 lg:gap-0">
+        <div className="container max-w-[90%] lg:max-w-7xl m-auto py-16 lg:py-20 xl:pl-24 lg:pl-16 flex flex-col-reverse lg:flex-row items-center gap-5 lg:gap-0">
           <div className="space-y-6 lg:max-w-[40%] flex flex-col items-center lg:items-start">
             <h2 className="text-2xl lg:text-4xl xl:text-5xl text-center lg:text-left">
               Supporting <br /> up-and-coming <br /> high-impact devs
