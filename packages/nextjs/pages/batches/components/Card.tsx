6491689aa14031d936f47c257824d9d749a00d04
phipsae <philip.krause88@yahoo.de>
2025-01-06 19:07:15 +0400

    add line spacing to card

--- packages/nextjs/pages/batches/components/Card.tsx
+++ packages/nextjs/pages/batches/components/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 export const Card = ({ icon, title, description }: CardProps) => {
   return (
     <div className="bg-white/60 rounded-lg p-4 shadow-sm transition-shadow">
-      <h3 className="font-bold mb-2 mt-2">
+      <h3 className="font-bold mb-2 mt-2 leading-relaxed">
         {icon} {title}
       </h3>
       <p className="text-gray-700 mb-0">{description}</p>
