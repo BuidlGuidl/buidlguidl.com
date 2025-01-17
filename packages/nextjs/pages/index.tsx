c85b45b51e851df9319864066cfda943e2ba91a9
Andrea Albiac <andrealbiac@gmail.com>
2025-01-17 17:55:36 +0100

    Added new image

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -25,10 +25,10 @@ const Home: NextPage<{
     <>
       <MetaHeader />
       {/* Hero section with header */}
-      <div className="relative h-[70vh] md:min-h-screen flex flex-col">
+      <div className="relative h-[60vh] flex flex-col">
         <div className="absolute h-1/4 w-full top-0 left-0 hero-top-gradient"></div>
         <Header />
-        <div className="bg-[url(/assets/hero.png)] bg-[#EFFBCA] bg-cover md:bg-center bg-[position:40%_0] flex-grow">
+        <div className="bg-[url(/assets/hero-new.png)] bg-[#EFFBCA] bg-cover md:bg-center bg-[position:40%_0] flex-grow">
           <div className="flex justify-center">
             <h1 className="text-center z-10 text-2xl max-w-xs lg:text-5xl lg:mt-8 lg:max-w-2xl px-3">
               Learn, build, and thrive on Ethereum
